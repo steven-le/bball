a1b5ce86a69f0e1ae8073ef0e73ad07887b88d8b
Steven L <steven@semirelevant.com>
2013-02-13 02:52:18 -0800

    Changed GemFile to use Postgres instead of Sqlite3. Added more seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,18 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+user1 = User.find_or_create_by_email :name => 'user1', :email => 'user1@test.com', :password => 'testtest', :password_confirmation => 'testtest'
+puts 'user: ' << user1.name
+user1.add_role :user
+user2 = User.find_or_create_by_email :name => 'user2', :email => 'user2@test.com', :password => 'testtest', :password_confirmation => 'testtest'
+puts 'user: ' << user2.name
+user2.add_role :user
+user3 = User.find_or_create_by_email :name => 'user3', :email => 'user3@test.com', :password => 'testtest', :password_confirmation => 'testtest'
+puts 'user: ' << user3.name
+user3.add_role :user
+user4 = User.find_or_create_by_email :name => 'user4', :email => 'user4@test.com', :password => 'testtest', :password_confirmation => 'testtest'
+puts 'user: ' << user4.name
+user4.add_role :user
+user5 = User.find_or_create_by_email :name => 'user5', :email => 'user5@test.com', :password => 'testtest', :password_confirmation => 'testtest'
+puts 'user: ' << user5.name
+user5.add_role :user
